20b5d8a836f3b142bc502f46f76e24182280ebeb
Deepak Chaurasiya <86309092+GaryMcKinnoDeepak@users.noreply.github.com>
2024-02-10 12:13:02 +0530

    Animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,15 @@
 var tl = gsap.timeline()
 
-tl.from("#page1",{
+tl.to("#page1",{
     y:"100vh",
+    scale:0.5,
+    duration:0
+
+})
+
+tl.to("#page1",{
+    y:"0vh",
+    duration:1,
+    delay:1
 })
 
